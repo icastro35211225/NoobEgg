301b9dda35cf152353abba134c2e09852ac5dfb3
julio valdes <juliovaldescava@gmail.com>
2022-03-05 12:15:49 -0600

    added necessary files

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header(props) {
+    
+    return (
+        <header className="row block center">
+            <div>
+                <h1 >UTHRIFTSA</h1> 
+            </div>
+            <div>
+                <a href="#/cart">Shopping Cart</a>
+            </div>
+        </header>
+    )
+}
