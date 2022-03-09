5620830784e654cfa21ac4a4384a247988676d92
julio valdes <juliovaldescava@gmail.com>
2022-03-09 13:37:49 -0600

    ADDED REACT ROUTER
    -added react-router-dom v6.2.2
    -added to App.js
       -imported routes and route to have different pages as routes
        -implemented in code
    -imported BrouserRouter to index.js
    -added a pages folder
        -added signup page and implemented to App.js

--- src/App.js
+++ src/App.js
@@ -1,90 +1,24 @@
 //import React, { useState, useEffect } from "react";
 import './App.css';
+import { Route, Routes } from "react-router-dom";
 //import Axios from 'axios';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Signup from './pages/Signup';
+
 
 function App() {
-
-  // const [itemName, setItemName] = useState('');
-  // const [itemDescription, setItemDescription] = useState('');
-  // const [itemList, setItemlist] = useState([]);
-  // const [newDescription, setNewDescription] = useState("");
-
-  // //ec2-3-93-234-9.compute-1.amazonaws.com
-  // //localhost
-
-  // useEffect(()=> {
-  //   Axios.get('http://ec2-3-93-234-9.compute-1.amazonaws.com:3000/api/get').then((response)=> {
-  //     console.log(response.status)
-  //     setItemlist(response.data);
-  //     //console.log(response.data);
-  //   })
-  // }, [])
-
-  // const submitItem = () => {
-  //   Axios.post('http://ec2-3-93-234-9.compute-1.amazonaws.com:3000/api/insert', {
-  //     itemName: itemName, 
-  //     itemDescription: itemDescription
-  //   });
-
-  //     setItemlist([...itemList, 
-  //       {itemName: itemName, itemDescription: itemDescription},
-  //     ]);
-  // };
-
-  // const deleteReview = (item) => {
-  //   Axios.delete(`http://ec2-3-93-234-9.compute-1.amazonaws.com:3000/api/delete/${item}`);
-
-  //   // setItemlist([...itemList, 
-  //   //   {itemName: itemName, itemDescription: itemDescription},
-  //   // ]); TRYING TO UPDATE THE LIST WITHOUT HAVING TO REFRESH PAGE
-    
-  // }
-
-  // const updateItem = (item) => {
-  //   Axios.put("http://ec2-3-93-234-9.compute-1.amazonaws.com:3000/api/update", {
-  //     itemName: item, 
-  //     itemDescription: newDescription
-  //   });
-    
-  //   setNewDescription("");
-
-  // }
-
   return (
     <div className="App">
       <Header></Header>
 
       <div className="form">
-        <Home></Home>
-        {/*<label>Item Name</label>
-        <input type="text" name="itemName" onChange={(e)=> {
-          setItemName(e.target.value)
-        }}/>
-        <label>Item Description</label>
-        <input type="text" name="description" onChange={(e)=> {
-          setItemDescription(e.target.value)
-        }}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />}/>    
+        </Routes>
 
-        <button onClick={submitItem}>Submit</button>
-         <Input></Input> 
-
-        {itemList.map((val)=> {
-          return (
-          <div className="card">
-            <h1>{val.itemName}</h1>
-            <p>{val.itemDescription}</p>
-
-            <button onClick={()=> {deleteReview(val.itemName)}}>Delete</button>
-            <input type="text" id="updateInput" onChange={(e)=> {
-              setNewDescription(e.target.value)
-            }}></input>
-            <button onClick={()=> {updateItem(val.itemName)}}>Update</button>
-            <button>Add to Cart</button>
-          </div>
-          );
-        })}INPUT COMPONENT DOES NOT WORK*/}
+        
       </div>
     </div>
   );
---
+++ src/pages/Signup.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Signup(props) {
+    
+    return (
+        <signup>
+            <h2>Make an account</h2>
+        </signup>
+    )
+}
