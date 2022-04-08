9a9e490ab490a6632e2bebe029837b389b5a24b3
julio valdes <juliovaldescava@gmail.com>
2022-04-08 13:52:05 -0500

    idk

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from './pages/Login';
 import Footer from './components/Footer';
 import AddItem from './pages/AddItem';
 import ShoppingCart from './pages/ShoppingCart';
+import Product from "./components/Product";
 
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
           <Route path="/login" element={<Login />}/>
           <Route path="/admin/additem" element={<AddItem />}/>
           <Route path="/cart" element={<ShoppingCart />}/>
+          <Route path="/product" element={<Product />}/>
         </Routes>
       </div>
       <Footer />
---
+++ src/components/Product.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import { useNavigate } from "react-router-dom";
+import '../App.css';
+
+export default function Product(props) {
+
+    const navigate = useNavigate();
+
+    const {product} = props;
+
+    return (
+            // <div key={product.productID} className="card">
+            //   <h1>{product.name}</h1>
+            //   <p>{product.desc}</p>
+            //   <p>{product.price}</p>
+            //   <p>Stock: {product.quantity}</p>
+
+            //   {/* <button onClick={()=> {deleteReview(val.name)}}>Delete</button>
+            //   <input type="text" id="updateInput" onChange={(e)=> {
+            //     setNewDescription(e.target.value)
+            //   }}></input>
+            //   <button onClick={()=> {updateItem(val.desc)}}>Update</button> */}
+            //   <button>Go to Page</button>
+            // </div>
+            <h1>{product.name}</h1>
+    );
+}
