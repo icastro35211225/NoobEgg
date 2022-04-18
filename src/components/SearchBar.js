075f1294821db6451fe5f776689723247e2fe8f4
MatthewH <matthewahernandez96@yahoo.com>
2022-04-18 14:38:31 -0500

    Get items to SearchBar
    Deleted data parameter from SearchBar
    
    Added itemList state
    Added effect to get itemList
    
    Added placeholder text to textbox

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,11 +3,19 @@ import Axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import '../App.css'; 
 
-function SearchBar({placeholder, data}) { 
+function SearchBar({placeholder}) { 
+    const [itemList, setItemlist] = useState([]);
+
+    useEffect(()=> {
+        Axios.get('http://ec2-3-93-234-9.compute-1.amazonaws.com:3000/api/get').then((response)=> {
+          setItemlist(response.data);
+        }) 
+    }, []) 
+
     return ( 
         <div className="searchbar"> 
             <div className="input">
-                <input type="text" /> 
+                <input type="text" placeholder={placeholder} /> 
             </div> 
             <div className="output"></div> 
         </div>
