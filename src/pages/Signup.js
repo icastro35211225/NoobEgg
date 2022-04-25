51e1825e4bd9c20051e2049339448ed1a99f220b
julio valdes <juliovaldescava@gmail.com>
2022-04-25 00:18:52 -0500

    Changes to Sign up page
    Added input checking and checking if emails are already used

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,34 +4,46 @@ import Axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye,faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import { Button, Card, Col } from "react-bootstrap";
+import { Button, Toast, ToastContainer, Col } from "react-bootstrap";
 const visible = <FontAwesomeIcon icon={faEye} />;
 const notVisible = <FontAwesomeIcon icon={faEyeSlash} />
 
 export default function Signup(props) {
 
     const navigate = useNavigate();
-
+    const [errShow, setErrShow] = useState(false);
+    const [emailUsed, setEmailUsed] = useState(false);
     const [fName, setFirstName] = useState('');
     const [lName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [address, setAddress] = useState('');
     const [passwordShown, setPasswordShown] = useState(false);
+    const [accounts, setAccounts] = useState();
     const togglePasswordVisiblity = () => {
         setPasswordShown(passwordShown ? false : true);
     };
 
     //FIXME: SHOULD ADD EMAIL VALIDATION (is email in right format?)
     //FIXME: CHECK IF EMAIL IS ALREADY USED WHEN CLICKING CREATE ACCOUNT
+    const getAccounts = async () => {
+        let tempAccounts = await Axios.get('http://ec2-3-93-234-9.compute-1.amazonaws.com:3000/api/getAllAccounts');
+        setAccounts(tempAccounts.data);
+    }
 
     const submitInfo = () => {
         
-        //FOR TESTING
-        // console.log(fName);
-        // console.log(lName);
-        // console.log(email);
-        // console.log(password);
+        if(fName === '' || lName === '' || email === '' || password === '' || address === ''){
+            setErrShow(true);
+            return;
+        }
+
+        for(let i = 0; i < accounts.length; i++){
+            if(email === accounts[i].Email){
+                setEmailUsed(true);
+                return;
+            }
+        }
 
         Axios.post('http://ec2-3-93-234-9.compute-1.amazonaws.com:3000/api/signup', {
           fName: fName,
@@ -42,20 +54,30 @@ export default function Signup(props) {
         });
 
         navigate('/login');
-
-        //clearFields();
-
     };
-    
-    const clearFields = () => {
-        setFirstName("");
-        setLastName("");
-        setEmail("");
-        setPassword("");
-    }
+
+    useEffect(() => {
+        getAccounts();
+    }, [])
     
     return (
         <signup>
+            <ToastContainer className="p-3" position={'top-end'} >
+                <Toast show={errShow} onClose={() => setErrShow(false)} bg={'danger'} delay={5000} autohide>
+                    <Toast.Header >
+                        <strong className="me-auto">Error</strong>
+                    </Toast.Header>
+                    <Toast.Body className="text-white">Please enter information for all fields.</Toast.Body>
+                </Toast>
+            </ToastContainer>
+            <ToastContainer className="p-3" position={'top-end'} >
+                <Toast show={emailUsed} onClose={() => setEmailUsed(false)} bg={'danger'} delay={5000} autohide>
+                    <Toast.Header >
+                        <strong className="me-auto">Error</strong>
+                    </Toast.Header>
+                    <Toast.Body className="text-white">This email is already being used.</Toast.Body>
+                </Toast>
+            </ToastContainer>
             <div className="login-wrapper">
                 <h2>SIGN UP</h2>
                 <div className="form-group">
