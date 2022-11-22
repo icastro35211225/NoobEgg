323f5abe08d6aaecd47523f8102d0cf31fdedc23
Ivan Castro <icastro3521@yahoo.com>
2022-11-22 09:05:10 -0600

    Changing search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,63 +1,74 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import '../App.css'; 
+import "../App.css";
 import { Card, Col } from "react-bootstrap";
 
-function SearchBar({placeholder}) { 
+function SearchBar({ placeholder }) {
+  let navigate = useNavigate();
+  const [allItems, setAllItems] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
 
-    let navigate = useNavigate();
-    const [allItems, setAllItems] = useState([]);
-    const [filteredList, setFilteredList] = useState([]); 
+  useEffect(() => {
+    Axios.get("https://api-noobegg.up.railway.app/api/get").then((response) => {
+      setAllItems(response.data);
+    });
+  }, []);
 
-    useEffect(()=> {
-        Axios.get('https://api-noobegg.up.railway.app/api/get').then((response)=> {
-          setAllItems(response.data);
-        }) 
-    }, []) 
+  const Filter = (event) => {
+    const searchWord = event.target.value;
+    // const filteredList = [];
 
-    const Filter = (event) => { 
-        const searchWord = event.target.value; 
-        // const filteredList = []; 
+    if (searchWord === "") {
+      setFilteredList([]);
+      // filteredList = allItems;
+    } else {
+      const filterNew = allItems.filter((product) => {
+        return product.ProductName.toUpperCase().includes(
+          searchWord.toUpperCase()
+        );
+      });
 
-        if (searchWord === "") { 
-            setFilteredList([]); 
-            // filteredList = allItems; 
-        } 
-        else { 
-            const filterNew = allItems.filter((product) => { 
-                return product.ProductName.toUpperCase().includes(searchWord.toUpperCase()); 
-            }); 
+      setFilteredList(filterNew);
 
-            setFilteredList(filterNew); 
-
-            /* filteredList = allItems.filter((product) => { 
+      /* filteredList = allItems.filter((product) => { 
                 return product.ProductName.toUpperCase().includes(searchWord.toUpperCase()); 
             }) */
-        } 
+    }
 
-        // NOTE: this probably doesn't even work anyway, need to use set function
-        // currentList = filteredList; 
-    }; 
+    // NOTE: this probably doesn't even work anyway, need to use set function
+    // currentList = filteredList;
+  };
 
-    return ( 
-        <div className="searchbar"> 
-            <div className="input"> 
-                <input type="text" placeholder={placeholder} onChange={Filter} /> 
-            </div> 
-            {filteredList.length > 0 && ( 
-                <div className="output">
-                    {filteredList.slice(0, 10).map((product) => { 
-                        return ( 
-                            <Card className="product" target="_blank"> 
-                                <Col><button className="productButton" onClick={() => navigate("/productscreen", { state: { id: product.productID } })}><h5>{product.name}</h5></button></Col> 
-                            </Card> 
-                        ); 
-                    })}
-                </div> 
-            )}
-        </div> 
-    )
-} 
+  return (
+    <div className="searchbar">
+      <div className="input">
+        <input type="text" placeholder={placeholder} onChange={Filter} />
+      </div>
+      {filteredList.length > 0 && (
+        <div className="output">
+          {filteredList.slice(0, 10).map((product) => {
+            return (
+              <Card className="product" target="_blank">
+                <Col>
+                  <button
+                    className="productButton"
+                    onClick={() =>
+                      navigate("/productscreen", {
+                        state: { id: product.productID },
+                      })
+                    }
+                  >
+                    <h5>{product.ProductName}</h5>
+                  </button>
+                </Col>
+              </Card>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
 
-export default SearchBar
+export default SearchBar;
